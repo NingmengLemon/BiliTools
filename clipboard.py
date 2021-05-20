26d0e0233f534c7c15947d80558d356dfe01113f
NingmengLemon <1435439745@qq.com>
2021-05-21 03:53:00 +0800

    v.2.0.0_Dev01

---
+++ clipboard.py
@@ -0,0 +1,19 @@
+import sys  
+import os.path  
+import win32clipboard as w    
+import win32con  
+import win32api  
+def getText():#读取剪切板  
+    w.OpenClipboard()
+    try:
+        d = w.GetClipboardData(win32con.CF_TEXT)
+    except:
+        return ''
+    else:
+        w.CloseClipboard()  
+        return d.decode('utf-8','ignore')
+def setText(aString):#写入剪切板  
+    w.OpenClipboard()  
+    w.EmptyClipboard()  
+    w.SetClipboardText(aString)  
+    w.CloseClipboard() 
