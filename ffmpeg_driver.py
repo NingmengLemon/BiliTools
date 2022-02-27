664ee031fa9372228294359033a1f322fade7df2
NingmengLemon <1435439745@qq.com>
2022-02-27 14:27:35 +0800

    2022.2.27
    日常修Bug

--- ffmpeg_driver.py
+++ ffmpeg_driver.py
@@ -2,7 +2,7 @@ import os
 
 def merge_media(audio_file,video_file,output_file): #传入时要带后缀
     assert not bool(os.popen('ffmpeg.exe -nostdin -hide_banner -i "{}" -i "{}" -vcodec copy -acodec copy "{}"'.format(audio_file,video_file,output_file)).close()),\
-           '混流失败: "{}"&"{}"->"{}" with vcodec "{}"'.format(video_file,audio_file,output_file,video_encoding)
+           '混流失败: "{}"&"{}"->"{}"'.format(video_file,audio_file,output_file,video_encoding)
 
 def convert_audio(inputfile,outfile=None,audio_format='mp3',quality='320k'):#outfile的后缀名由audio_format决定
     if outfile:
