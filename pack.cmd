2eb4714a2d9a050551c3eca0b9a1de000fd3e9f0
LemonyNingmeng <1435439745@qq.com>
2023-09-18 23:14:29 +0800

    small update
    change hot-key
    update devlog
    beautify pack.cmd
    modify tips

--- pack.cmd
+++ pack.cmd
@@ -1,7 +1,12 @@
 @cd %~dp0
+@echo ==================== Preparing =======================
 @pipenv install --skip-lock
 @pipenv run pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 @pipenv run pip install pyinstaller
 @pipenv run pip install -r requirements.txt
-@pipenv run pyinstaller -w --noconfirm -i "./favicon.ico" -D "./bilitools.py"
+@echo ================== Generating: DIR ===================
+@pipenv run pyinstaller -w --noconfirm -i "./favicon.ico" --onedir "./bilitools.py"
+@echo ================= Generating: FILE ===================
+@pipenv run pyinstaller -w --noconfirm -i "./favicon.ico" --onefile "./bilitools.py"
+@echo ======================== DONE ========================
 @pause
